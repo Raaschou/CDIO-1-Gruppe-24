fafe2c79ac02aa26061d74428da8abf6208223ec
Raaschou <168561326+Raaschou@users.noreply.github.com>
2024-09-26 11:37:41 +0200

    Test Github

---
+++ CDIO1_main.java
@@ -0,0 +1,19 @@
+Class Dice {
+    public static void main (String[] args){
+        
+
+        //Sæt player score til 0
+        //evt egen player class
+        var player1 = 0
+        var player2 = 0
+
+
+        //Terning class
+        java.util.Random
+
+        
+
+
+
+    }
+}
